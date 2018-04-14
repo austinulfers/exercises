3a255bd574e213a2a4a35d980325ab3c7de28f93
Austin Ulfers <austinulfers@gmail.com>
2018-04-13 19:39:09 -0700

    worked on various exercises

--- ch7-vectors/exercise-1/exercise.R
+++ ch7-vectors/exercise-1/exercise.R
@@ -1,6 +1,6 @@
 # Exercise 1: creating and operating on vectors
 
-# Create a vector `names` that contains your name and the names of 2 people 
+# Create a vector `names` that contains your name and the names of 2 people
 # next to you. Print the vector.
 names <- c("James", "Joey")
 
@@ -13,7 +13,7 @@ length(n)
 # Add 1 to each element in `n` and print the result
 n <- n + 1
 
-# Create a vector `m` that contains the numbers 10 to 1 (in that order). 
+# Create a vector `m` that contains the numbers 10 to 1 (in that order).
 # Hint: use the `seq()` function
 m <- rev(seq(10:1))
 
@@ -22,13 +22,13 @@ m - n
 
 # Use the `seq()` function to produce a range of numbers from -5 to 10 in `0.1`
 # increments. Store it in a variable `x_range`
-x_range <- seq(-5,10,.1)
+x_range <- seq(-5, 10, .1)
 
-# Create a vector `sin_wave` by calling the `sin()` function on each element 
+# Create a vector `sin_wave` by calling the `sin()` function on each element
 # in `x_range`.
 sin_wave <- sin(x_range)
 
-# Create a vector `cos_wave` by calling the `cos()` function on each element 
+# Create a vector `cos_wave` by calling the `cos()` function on each element
 # in `x_range`.
 cos_wave <- cos(x_range)
 
